fdf90308012f50103949a5b9f44e4ace893879f6
KIMDOWON <dw.kim@theinfoin.com>
2023-11-22 16:37:21 +0900

    Feat : test 데이터 수정

--- src/components/ErrorComponet.tsx
+++ src/components/ErrorComponet.tsx
@@ -1,6 +1,5 @@
-
 const ErrorComponet = () => {
-  return <h1>이 컴퍼넌트는 충돌되었습니다</h1>;
+    return <h1>이 컴퍼넌트는 충돌되었습니다</h1>;
 };
 
 export default ErrorComponet;
--- src/components/db.ts
+++ src/components/db.ts
@@ -1,10 +1,10 @@
 export const users = [
-  {
-    id: 1,
-    name: "koko",
-  },
-  {
-    id: 2,
-    name: "lolo",
-  },
+    {
+        id: 1,
+        name: 'koko',
+    },
+    {
+        id: 2,
+        name: 'lolo',
+    },
 ];
